654a2293f6b3992d754901eea8902249c1faf0d5
PranjalPiya <pranjalpiya10@gmail.com>
2024-12-04 13:08:29 +1100

    week 3 - css name change

--- Week 3/public/js/colorChange.js
+++ Week 3/public/js/colorChange.js
@@ -1,23 +1,23 @@
 
+// // $(document).ready(function () {
+// //     $(".button1").click(function () {
+// //         $(this).css('color', 'red')
+// //     });
+// //     // $('.button1').css('color', 'red');
+// // });
+
+
+// // selector for jquery
+// // script.js
+
+// // $(document).ready(function () {
+// //     $(".button1").css("color", "red");
+// // });
+
+
 // $(document).ready(function () {
+//     // Event listener for button click
 //     $(".button1").click(function () {
-//         $(this).css('color', 'red')
+//         $(this).css("background-color", "red");
 //     });
-//     // $('.button1').css('color', 'red');
 // });
-
-
-// selector for jquery
-// script.js
-
-// $(document).ready(function () {
-//     $(".button1").css("color", "red");
-// });
-
-
-$(document).ready(function () {
-    // Event listener for button click
-    $(".button1").click(function () {
-        $(this).css("background-color", "red");
-    });
-});
